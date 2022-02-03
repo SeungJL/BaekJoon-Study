210241cef8d683e9046e1000f2b6e913450097c5
suddiyo <suddii01@sju.ac.kr>
2022-02-03 19:42:21 +0900

    수경

--- Third/2847/수경.cpp
+++ Third/2847/수경.cpp
@@ -1,4 +1,21 @@
-//
-// Created by 안수경 on 2022/02/02.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    int x[100];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &x[i]);
+    }
+    int result = 0;
+    for (int i = N - 1; i > 0; i--) {
+        if (x[i - 1] - x[i] >= 0) {
+            result += x[i - 1] - x[i] + 1;
+            x[i - 1] -= x[i - 1] - x[i] + 1;
+        }
+    }
+    printf("%d", result);
+    return 0;
+}
