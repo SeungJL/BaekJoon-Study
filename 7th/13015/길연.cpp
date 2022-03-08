e4b9fb8cf09bd9e76b988185aa18713036c52b97
gilyeon00 <52391627+gilyeon00@users.noreply.github.com>
2022-03-08 16:42:51 +0900

    7th 길연

--- 7th/13015/길연.cpp
+++ 7th/13015/길연.cpp
@@ -7,15 +7,13 @@
 
 using namespace std;
 
-
-
 int main() {
 	
 	int n;
 	cin >> n;
 
 	// 별은 총 2n-1줄로 구성됨. 위에 n-1 가운데 1 아래 n-1
-	for (int i = 0; i < 2 * n - 1; i++) {
+	for (int i = 0; i <= 2 * n - 1; i++) {
 
 		if (i == 0 || i == 2 * n - 1) {  // 맨 윗줄 || 맨 아랫줄
 			for (int j = 0; j < n; j++) cout << "*";
@@ -26,24 +24,52 @@ int main() {
 		}
 
 		if (i == n - 1) {  // 가운데
-			for (int j = 0; j < n - 1; j++) cout << " ";
+			for (int j = 1; j <= n - 1; j++) cout << " ";
 			cout << "*";
 			for (int j = 0; j < n - 2; j++) cout << " ";
-			cout << " ";
+			cout << "*";
 			for (int j = 0; j < n - 2; j++) cout << " ";
 			cout << "*" << endl;
 			continue;
 		}
 
-		if (i < n) {   // 가운데 기준으로 윗 부분
+		if (i < n - 1) {   // 가운데 기준으로 윗 부분
 			for (int j = 1; j <= i; j++) cout << " ";
 			cout << "*";
-			for (int j = 1; j < n - 2; j++) cout << " ";
+
+			for (int j = 0; j < n - 2; j++) cout << " "; // 불변
 			cout << "*";
-			for (int j = n; j >= 1; j--) cout << " ";
-			cout << "*" << endl;
-			for (int j = 0; j < n - 2; j++) cout << " ";
+
+			for (int j = abs(i-3); j > 0; j--) cout << " ";  // 가변  5 3 1
+			cout << " ";
+			for (int j = abs(i - 3); j > 0; j--) cout << " ";
 			cout << "*";
+
+			for (int j = 0; j < n - 2; j++) cout << " "; // 불변
+			cout << "*";
+
+			for (int j = 1; j <= i; j++) cout << " ";
+			cout << endl;
+		}
+
+		if (i > n) {  // 가운데 기준으로 아랫 부분
+			for (int j = abs(i-n-4); j > 0; j--) cout << " ";
+			cout << "*";
+			
+			for (int j = 0; j < n - 2; j++) cout << " "; // 불변
+			cout << "*";
+
+
+			for (int j = i - 6; j > 0; j--) cout << " ";  // 가변  1 3 5
+			cout << " ";
+			for (int j = i - 6; j > 0; j--) cout << " ";
+			cout << "*";
+
+			for (int j = 0; j < n - 2; j++) cout << " "; // 불변
+			cout << "*";
+
+			for (int j = abs(i - n - 4); j > 0; j--) cout << " ";
+			cout << endl;
 		}
 	} 
 
