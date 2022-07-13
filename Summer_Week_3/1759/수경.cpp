3405011bf1f4f31e616f1d8ec9ec325928ed6307
suddiyo <suddii01@sju.ac.kr>
2022-07-13 16:29:26 +0900

    ㅅㅜ경

--- Summer_Week_3/1759/수경.cpp
+++ Summer_Week_3/1759/수경.cpp
@@ -1,4 +1,58 @@
-//
-// Created by 안수경 on 2022/07/11.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int n, m;
+
+bool arr[26] = {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1};
+
+
+void dfs(string s, int now, int depth, vector<char> alphabets) {
+
+    if (depth == n) {
+        int za = 0;
+        int mo = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (arr[s[i] - 97]) {
+                za++;
+            } else {
+                mo++;
+            }
+        }
+        if (za >= 2 && mo >= 1) {
+            cout << s << endl;
+        }
+        return;
+    }
+
+    for (int i = now + 1; i <= m - n + depth; i++) {
+        dfs(s + alphabets[i], i, depth + 1, alphabets);
+    }
+
+
+}
+
+
+int main() {
+
+
+    cin >> n >> m;
+
+
+    vector<char> alphabets(m);
+
+
+    for (int i = 0; i < m; i++) {
+        cin >> alphabets[i];
+    }
+
+    sort(alphabets.begin(), alphabets.end());
+
+    string s = "";
+
+    for (int i = 0; i <= m - n; i++) {
+        dfs(s + alphabets[i], i, 1, alphabets);
+    }
+    return 0;
+}
--- Summer_Week_3/7562/수경.cpp
+++ Summer_Week_3/7562/수경.cpp
@@ -1,4 +1,53 @@
-//
-// Created by 안수경 on 2022/07/11.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+int graph[300][300];
+int dir[8][2] = {{1,  2},
+                 {2,  1},
+                 {1,  -2},
+                 {2,  -1},
+                 {-1, 2},
+                 {-2, 1},
+                 {-1, -2},
+                 {-2, -1}};
+int n, sx, sy, fx, fy;
+queue <pair<int, int>> q;
+
+void bfs() {
+    q.push({sx, sy});
+
+    while (!q.empty()) {
+        int x, y;
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 8; i++) {
+            int xx = x + dir[i][0];
+            int yy = y + dir[i][1];
+            if (xx >= 0 && xx < n && yy >= 0 && yy < n && !graph[xx][yy]) {
+                q.push({xx, yy});
+                graph[xx][yy] = graph[x][y] + 1;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cin >> sx >> sy >> fx >> fy;
+        graph[sx][sy] = 1;
+        bfs();
+        cout << graph[fx][fy] - 1 << '\n';
+        memset(graph, 0, sizeof(graph));
+
+    }
+
+    return 0;
+}
