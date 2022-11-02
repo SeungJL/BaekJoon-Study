616a8f82879cba8202728ae904f90ef6ee107660
banana970 <84257439+banana970@users.noreply.github.com>
2022-11-02 15:13:06 +0900

    Update 승주.cpp

--- 10.24~10.30/1976/승주.cpp
+++ 10.24~10.30/1976/승주.cpp
@@ -1,55 +1,40 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <set>
 using namespace std;
 
 int N, M;
-int arr[201][201] = {};
+bool arr[201][201] = {};
 bool visit[201] = {};
-vector<int> V;
+set <int> s;
 
 void DFS(int start) {
+	s.insert(start);
 	visit[start] = 1;
-	for (int i = 0; i < N; i++) {
-		if (visit[i] == 0 && arr[start][i] == 1) {
+	for (int i = 1; i <= N; i++) {
+		if (visit[i]==0&&arr[start][i] == 1) {
 			visit[i] = 1;
-			V.push_back(i);
 			DFS(i);
 		}
 	}
 }
 
 int main() {
-	cin >> N;
-	cin >> M;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
+	cin >> N >> M;
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
 			cin >> arr[i][j];
 	}
-	int routine[1001];
-	for (int i = 0; i < M; i++)
-		cin >> routine[i];
-	sort(routine, routine + M);
-	V.push_back(routine[0] - 1);
+	int temp;
+	cin >> temp;
+	DFS(temp);
 
-	DFS(routine[0] - 1);
-	sort(V.begin(), V.end());
-
-	int j = 0;
-	for (int i = 0; i < M; i++) {
-		if (routine[i] - 1 == V[j]) {
-			continue;
-		}
-		else if (j < V.size() - 1 && routine[i] - 1 == V[j + 1]) {
-			j++;
-		}
-		else {
-			cout << "NO";
-			return 0;
-		}
+	set <int>::iterator iter;
+	bool flag = 0;
+	for (int i = 0; i < M - 1; i++) {
+		cin >> temp;
+		if (s.find(temp) == s.end()) flag = 1;
 	}
-	cout << "YES";
-
+	if (!flag) cout << "YES";
+	else cout << "NO";
 }
