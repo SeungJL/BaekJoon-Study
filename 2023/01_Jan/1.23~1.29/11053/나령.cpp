5a4f8e7691d5a32003ca36093ab0064760cf3854
Na Ryeong Lee <thisisme20@daum.net>
2023-01-30 20:25:42 +0900

    Update 나령.cpp

--- 2023/01_Jan/1.23~1.29/11053/나령.cpp
+++ 2023/01_Jan/1.23~1.29/11053/나령.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
+#define MAX 1010
 using namespace std;
+ 
+int Max(int A, int B) { if (A > B) return A; return B; }
+ 
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
 
-int main() {
-	int dp[10000][10] = {};
-	int n;
-	int sum = 0;
+  //freopen("Input.txt", "r", stdin);
+  int n;
+  int arr[MAX];
+  int DP[MAX];
+  int answer = 0;
+  
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> arr[i];
 
-	cin >> n;
-
-	for (int i = 0; i < 10; i++) {
-        dp[1][i] = 1;
+  
+  for (int i = 1; i <= n; i++) {
+    DP[i] = 1;
+    for (int j = i - 1; j >= 1; j--) {
+      if (arr[i] > arr[j]) {
+        DP[i] = Max(DP[i], DP[j] + 1);
+      }
     }
-		
-	for (int j = 2; j <= n; j++) {
-		for (int i = 0; i < 10; i++) {
-			for (int k = 0; k <= i; k++) {
-				dp[j][i] += dp[j - 1][k];
-			}
-			dp[j][i] %= 10007;
-		}
-	}
-    
-	for (int i = 0; i < 10; i++) {
-        sum = (sum + dp[n][i]) % 10007;
-    }
-
-	cout << sum % 10007;
+      answer = Max(DP[i], answer);
+  }
+ 
+  cout << answer << "\n";
+ 
+  return 0;
 }
