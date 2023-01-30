133b9fa05076439f204e0a29f1fbb9ee1bfca850
Na Ryeong Lee <thisisme20@daum.net>
2023-01-30 20:27:09 +0900

    나령

---
+++ 2023/01_Jan/1.23~1.29/11053/나령.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAX 1010
+using namespace std;
+ 
+int Max(int A, int B) { if (A > B) return A; return B; }
+ 
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  //freopen("Input.txt", "r", stdin);
+  int n, answer = 0;
+  int arr[MAX];
+  int DP[MAX];
+  
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> arr[i];
+
+  
+  for (int i = 1; i <= n; i++) {
+    DP[i] = 1;
+    for (int j = i - 1; j >= 1; j--) {
+      if (arr[i] > arr[j]) {
+        DP[i] = Max(DP[i], DP[j] + 1);
+      }
+    }
+      answer = Max(DP[i], answer);
+  }
+ 
+  cout << answer << "\n";
+ 
+  return 0;
+}
