05853e67109dac60a9b9b082aa130f86bad2782b
Na Ryeong Lee <thisisme20@daum.net>
2023-02-15 03:20:42 +0900

    나령

--- 2023/02_Feb/2.6~2.12/17298/나령.cpp
+++ 2023/02_Feb/2.6~2.12/17298/나령.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int a[1000001];
 int ans[1000001];
-stack<int> s;
+stack<int> s; // 오큰수를 구하려고 하는 수의 인덱스(값X)
 
 int main(){
     int n;
-    cin>>n;
+    cin >> n;
     for(int i=0; i<n; i++) cin >> a[i];
 
     for(int i=0; i<n; i++) {
